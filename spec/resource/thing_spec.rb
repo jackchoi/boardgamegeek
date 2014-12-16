3766439549bd8901cf46a1b7992a19c22e4a5b57
Jack <jtestf+git@gmail.com>
2014-12-15 20:00:48 -0500

    Adding sample thing spec

--- spec/resource/thing_spec.rb
+++ spec/resource/thing_spec.rb
@@ -1,13 +1,11 @@
 require "spec_helper"
 
-describe "Thing" do
-  # subject(:content) do
-  #   VCR.use_cassette "thing_spec" do
-  #     BoardGameGeek::Thing.request(:id => 1)
+module BoardGameGeek
+  # describe Thing do
+  #   subject(:content) do
+  #     VCR.use_cassette "thing_spec-id-1" do
+  #       Thing.request(:id => 1)
+  #     end
   #   end
-  # end
-  # it "should be good stuff" do
-  #   p content
-  #   expect(content).to be_nil
   # end
 end
