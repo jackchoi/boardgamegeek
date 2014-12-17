9ea40c6fd5fed424ab3dc663f32a1593b4cf9da3
Jack <jtestf+git@gmail.com>
2014-12-16 19:32:45 -0500

    Fixing how Resource RSpec is testing

--- spec/resource/resource_spec.rb
+++ spec/resource/resource_spec.rb
@@ -1,26 +1,31 @@
 require "spec_helper"
 
 module BoardGameGeek
-  class ResourceImpl
-  end
-
   describe Resource do
-    before :each do
-      @resource_obj = ResourceImpl.new
-      @resource_obj.extend(Resource)
+
+    let(:resource_class) do
+      if BoardGameGeek.const_defined? :ResourceImpl
+        BoardGameGeek.send(:remove_const, :ResourceImpl)
+      end
+
+      class ResourceImpl
+        extend Resource
+      end
+
+      ResourceImpl
     end
 
     describe "#resource_name" do
-      it "defaults to the lowercase of the class name" do
-        expect(@resource_obj.resource_name).to eq("resourceimpl")
+      it "defaults to the lowercase of the classname when extended" do
+        expect(resource_class.resource_name).to eq("resourceimpl")
       end
     end
 
     describe "#resource_name=" do
       it "overrides the default resource name" do
         resource_name = "overriddenresource"
-        @resource_obj.resource_name = resource_name
-        expect(@resource_obj.resource_name).to eq(resource_name)
+        resource_class.resource_name = resource_name
+        expect(resource_class.resource_name).to eq(resource_name)
       end
     end
 
@@ -33,11 +38,11 @@ module BoardGameGeek
       end
 
       it "yields the parsed return data" do
-        expect{ |b| @resource_obj.request(:foo => "bar", &b) }.to yield_with_args(Hash)
+        expect{ |b| resource_class.request(:foo => "bar", &b) }.to yield_with_args(Hash)
       end
 
       it "returns the parsed return data" do
-        expect(@resource_obj.request(:foo => "bar")).to be_a(Hash)
+        expect(resource_class.request(:foo => "bar")).to be_a(Hash)
       end
     end
   end
